1cf4ad13d13768cadc9d1bc80592d063ea9d8fc6
kzhou2 <kzhou2@e.ntu.edu.sg>
2014-04-06 19:19:15 +0800

    wefwee

--- war/mastermind.js
+++ war/mastermind.js
@@ -17,7 +17,7 @@ colorImages  = ['redball.gif',    'blueball.gif',    'brownball.gif',
                 'greenball.gif',  'yellowball.gif',  'pinkball.gif' ];
 
 // --------------------------------------------------------
-// called when user clicks "New Gamedd".
+// called when user clicks "New Gameddxx".
 function newGame(){
     resetGame();
     gameNumber++;
